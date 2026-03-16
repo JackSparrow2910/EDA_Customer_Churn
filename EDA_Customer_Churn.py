7466a1e2ab2a8ff3041caa9359cc8eff24cdfae5
Volodymyr Vladyko <vladykovladimir233@gmail.com>
2026-03-16 12:16:59 +0200

    Check presence of duplicates in the table(There are no duplicates). And also use function describe() to watch data distribution

--- EDA_Customer_Churn.py
+++ EDA_Customer_Churn.py
@@ -4,12 +4,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('Customer_Churn_origin.csv')
-print(df.head())
-print(df.info())
 
 #replacing blank values with 0 because tenure is 0 so there are not records
 df["TotalCharges"] = df["TotalCharges"].replace(" ", "0")
 df["TotalCharges"] = df["TotalCharges"].astype("float")
 
+print(df)
+print()
+print(df.head())
+print()
+print(df.info())
+print()
+
 #There is no null values in table
-print(df.isnull().sum().sum())
+print(f"Count of null values: {df.isnull().sum().sum()}")
+print()
+
+print(df.describe())
+print()
+
+#There are no duplicates in whole table
+print(f"Count duplicates of whole table: {df.duplicated().sum()}")

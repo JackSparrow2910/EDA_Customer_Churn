726712a8ce348dacc30ce18a9db23dfb9b251c70
Volodymyr Vladyko <vladykovladimir233@gmail.com>
2026-03-16 12:03:24 +0200

    Replacing blank values with 0 in TotalCharges via Python (pandas)

--- EDA_Customer_Churn.py
+++ EDA_Customer_Churn.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('Customer Churn.csv')
+df = pd.read_csv('Customer_Churn_origin.csv')
 print(df.head())
 print(df.info())
+
+#replacing blank values with 0 because tenure is 0 so there are not records
+df["TotalCharges"] = df["TotalCharges"].replace(" ", "0")
+df["TotalCharges"] = df["TotalCharges"].astype("float")

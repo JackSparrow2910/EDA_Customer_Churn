0f3f398b1270f5ebd3d81ab058729a68a55d6ba5
Volodymyr Vladyko <vladykovladimir233@gmail.com>
2026-03-20 16:26:29 +0200

    After each graphics add summary about each it with commentary which starts with ###

--- EDA_Customer_Churn.py
+++ EDA_Customer_Churn.py
@@ -45,6 +45,7 @@ print(df.head())
 #gb = df.groupby('Churn').agg({'Churn':'count'})
 #plt.pie(gb['Churn'], labels=gb.index, autopct='%1.2f%%')
 #plt.title('Percentage of churned customers', fontsize=20)
+###from the given pie chart we can conclude that 26.54% of our customers have churned out.
 
 ##Countplot of churned customers by gender
 # plt.figure(figsize=(4,4))
@@ -78,10 +79,13 @@ print(df.head())
 # plt.ylabel("Churn (%)")
 # plt.xticks(rotation=0)
 # plt.legend(title='Churn', loc='upper right', bbox_to_anchor=(1.05, 1))
+###comparative a greater pecentage of people in senior citizen category have churned
 
 
 ##Histogram of churned customers by tenure
 # sns.histplot(x='tenure', data=df, bins=72, hue='Churn')
+### people who have used our services for a long time have stayed and people who have used our sevices
+### 1 or 2 months have churned
 
 
 ##Countplot of churned customers by Contract
@@ -89,6 +93,7 @@ print(df.head())
 # plt.title("Churn by Contracts")
 # ax=sns.countplot(x='Contract', data=df, hue='Churn')
 # ax.bar_label(ax.containers[0])
+###people who have month to month contract are likely to churn then from those who have 1 or 2 years or contract.
 
 
 ##Multiple countplot of churned customers by Service
@@ -117,7 +122,8 @@ print(df.head())
 #     fig.delaxes(axes[j])
 #
 # plt.tight_layout()
-
+###The majority of customers who do not churn tend to have services like PhoneService, InternetService (particularly DSL), and OnlineSecurity enabled.
+### For services like OnlineBackup, TechSupport, and StreamingTV, churn rates are noticeably higher when these services are not used or are unavailable.
 
 ##Countplot of churned customers by Payment Method
 plt.figure(figsize=(9,4))
@@ -126,6 +132,7 @@ ax=sns.countplot(x='PaymentMethod', data=df, hue='Churn')
 ax.bar_label(ax.containers[0])
 ax.bar_label(ax.containers[1])
 plt.xticks(rotation=0)
+###customer is likely to churn when he is using electronic check as a payment method.
 
 
 
